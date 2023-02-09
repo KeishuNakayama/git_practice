4fd53b0ee52724a1375fdf2eb796f33831058985
KeishuNakayama <nakayama.keishu@gmail.com>
2023-02-09 19:13:14 +0900

    introduce name

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World こんにちは、世界");
+        String name = "けいしゅう";
+        System.out.println("私は" + name + "です");
     } 
 }
